b8dc360267036614018ff86ec454b944bbcee549
Dragondiv1 <mohamed.oussaid@soprasteria.com>
2015-12-08 15:29:47 +0100

    Implementation de la classe Employe !

--- test_github_project_coursAdaming_master/src/com/adaming/bo/Employe.java
+++ test_github_project_coursAdaming_master/src/com/adaming/bo/Employe.java
@@ -8,22 +8,22 @@ public class Employe {
 	public Employe() {
 		super();
 	}
-	
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	public String getNom() {
 		return nom;
 	}
-	
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
+	
 	
 }
